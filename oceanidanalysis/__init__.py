b95cfac53684c79ad9dbc7e61096850fb68f7dc8
M J Stanway <m.j.stanway@alum.mit.edu>
2012-08-31 11:38:47 -0700

    package init script (basically a strawman for the structure I'm thinking of)

---
+++ oceanidanalysis/__init__.py
@@ -0,0 +1,18 @@
+
+## data reader
+# import data
+## common types of plots
+# import timeseries
+import maps
+## vehicle state, health, and performance
+# import lrauv
+# import lrauv.state
+# import lrauv.health
+# import lrauv.performance
+## ocean currents
+# import currents
+# import currents.surface
+# import currents.subsurface
+## fronts
+# import fronts
+
