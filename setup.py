82bdd2811a7f5a054e3fdf552700ddc8f7112d4a
M J Stanway <m.j.stanway@alum.mit.edu>
2012-09-12 13:00:53 -0700

    basic setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='oceanidanalysis',
+      version='0.0.1',
+      description='Tools and examples for working with MBARI LRAUV data.',
+      author='M J Stanway',
+      author_email='mjstanway@mbari.org',
+      url='',
+      packages=['oceanidanalysis'],
+     )
