f16befe4c0b3b1ff258f0f29dfd89295d35d45e2
Michael Jordan Stanway <m.j.stanway@alum.mit.edu>
2013-05-22 10:43:45 -0700

    removed separated modules from __init__.py

--- okeanidanalysis/__init__.py
+++ okeanidanalysis/__init__.py
@@ -5,16 +5,3 @@ import logs
 ## common types of plots
 # import timeseries
 import maps
-## vehicle state, health, and performance
-# import lrauv
-# import lrauv.state
-# import lrauv.health
-# import lrauv.performance
-## ocean currents
-import currents
-# import currents.surface
-# import currents.subsurface
-## fronts
-# import fronts
-## Empirical Orthogonal Functions
-import eof
