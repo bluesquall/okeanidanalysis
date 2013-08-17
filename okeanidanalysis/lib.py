56cf0c8db94e572f91f849163729b6ffcb13ea9f
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2013-08-16 23:31:01 -0700

    flip loadmtx dimensions for consistency with SF's matlab routine fort_read

--- okeanidanalysis/lib.py
+++ okeanidanalysis/lib.py
@@ -177,8 +177,10 @@ def loadmtx(filename):
 
     """
     with open(filename, 'rb') as f:
-        shape = np.fromfile(file=f, dtype=np.int32, count=2)[::-1]
-        return np.fromfile(file=f, dtype=np.float32).reshape(shape).T
+        shape = np.fromfile(file=f, dtype=np.int32, count=2)[::-1] 
+        # not sure why the .mtx file has its dimensions flipped, but this 
+        # reads consistent with SF's fort_read & fort_readColumns
+        return np.fromfile(file=f, dtype=np.float32).reshape(shape, order='F')
 
 
 def savemtx(a, filename):
