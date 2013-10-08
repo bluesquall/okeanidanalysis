557d4804c2a2060e5375221756514cdde8ad9334
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2013-10-08 15:05:27 -0700

    remove class & methods related to gviz_api
    The new shore logs are being incorporated into TethysDash, and including the gviz_api code here introduces an extra dependency that most people won't use. I don't think it should be in there for the public version of the tool, and if we reintroduce it in the future, we should do it in a way such that the extra dependency is only required if users specifically enable and load it.

--- okeanidanalysis/logs.py
+++ okeanidanalysis/logs.py
@@ -6,7 +6,6 @@ Generally useful methods that span across submodules.
 
 """
 
-# import cgi # do I actually need this anywhere?
 import datetime, itertools
 
 import numpy as np
@@ -14,8 +13,6 @@ import scipy as sp
 import h5py
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-
-import gviz_api
 
 import okeanidanalysis.lib as oalib
 
@@ -411,72 +408,3 @@ class OkeanidLog(h5py.File):
 #class VehicleLog(OkeanidLog):
 #class ShoreLog(OkeanidLog):
 #class ShipLog(OkeanidLog):
-
-class GVisLog(OkeanidLog):
-    """Provides extra methods to interface with Google Charts API.
-    
-    (Really just separated into its own class for taste.)
-    
-    """
-    
-    def timeseries_to_gviz_data_table(self, group, **kw):
-        """Create a DataTable for the Google Charts API.
-
-        """
-        kw.update(return_epoch=False,) # otherwise LoadData won't work
-        description = { 'group': ('number', group),
-                        'time': ('datetime', 'time') }
-        data = [{'group': float(v), 'time': t} for v,t 
-                in itertools.izip(*self.timeseries(group, **kw))]
-        table = gviz_api.DataTable(description)
-        table.LoadData(data)
-        return table
-        
-
-    def timeseries_to_json_response(self, form, group, **kw):
-        """Generate a full API-compliant response to serve timeseries data.
-
-        Parameters
-        ----------
-        form : an instance of cgi.FieldStorage() using psl cgi
-        
-        group : string
-            points to the timeseries of interest, e.g., sea_water_temperature
-
-        Returns
-        -------
-        response : string
-            contains the content header, separation, and google.visualization.Query.setResponse
-
-        References
-        ----------
-        https://developers.google.com/chart/interactive/docs/dev/implementing_data_source#responseformat
-
-        """
-        tqxstr = form.getfirst('tqx')
-        if not tqxstr:
-            tqx = {} # empty dict, and probably bad request...
-        else:
-            tqx = dict([p.split(':') for p in tqxstr.split(';')])
-
-        jsrkw = dict(columns_order=('time', 'group'), order_by='time',
-# XXX not handled yet...                responseHandler='google.visualization.Query.setResponse',
-                )
-
-        if 'version' in tqx: 
-            jsrkw.update(version=tqx['version'])
-        if 'reqId' in tqx: 
-            jsrkw.update(req_id=tqx['reqId'])
-        # TODO handle status other than OK...
-        # TODO handle sig... if sig in tqx: jsrkw.update(req_id=tqx['reqId'])
-        if 'out' in tqx: 
-            if tqx['out'] is not 'json': 
-                raise NotImplementedError
-        # TODO these are _supposed_ to be defined and handled...
-        #if 'responseHandler' in tqx: 
-        #    jsrkw.update(responseHandler=tqx['responseHandler'])
-        #if 'outFileName' in tqx: 
-        #    raise NotImplementedError
-        table = self.timeseries_to_gviz_data_table(group, **kw)
-        return table.ToJSonResponse(**jsrkw)
-    
